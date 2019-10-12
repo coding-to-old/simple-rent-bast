15154569bec92c5275fad8f75aa67a83b937f49b
ruirui.zheng@esrtech.cn <ruirui.zheng@esrtech.cn>
2019-10-12 17:37:55 +0800

    init

---
+++ rent-bast-plugin/src/main/java/rent/bast/UtilPlugin.java
@@ -0,0 +1,11 @@
+package rent.bast;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class UtilPlugin {
+    
+    
+}
